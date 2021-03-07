f6119b1707784ba4a3c5f2402880da67a870eb21
Kareem Stephan Skinner <kastsk@gmail.com>
2021-03-07 16:21:03 -0500

    Updated imgui location

--- src/Application.cpp
+++ src/Application.cpp
@@ -20,7 +20,7 @@
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
 
-// About Desktop OpenGL function loaders:
+//  About Desktop OpenGL function loaders:
 //  Modern desktop OpenGL doesn't have a standard portable header file to load OpenGL function pointers.
 //  Helper libraries are often used for this purpose! Here we are supporting a few common ones (gl3w, glew, glad).
 //  You may use another loader/header of your choice (glext, glLoadGen, etc.), or chose to manually implement your own.
@@ -46,15 +46,8 @@ using namespace gl;
 #include IMGUI_IMPL_OPENGL_LOADER_CUSTOM
 #endif
 
-// Include glfw3.h after our OpenGL definitions
+// Include glfw3.h after OpenGL definitions
 #include <GLFW/glfw3.h>
-
-// [Win32] Our example includes a copy of glfw3.lib pre-compiled with VS2010 to maximize ease of testing and compatibility with old VS compilers.
-// To link with VS2010-era libraries, VS2015+ requires linking with legacy_stdio_definitions.lib, which we do using this pragma.
-// Your own project should not be affected, as you are likely to link with a newer binary of GLFW that is adequate for your version of Visual Studio.
-#if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
-#pragma comment(lib, "legacy_stdio_definitions")
-#endif
 
 static void glfw_error_callback(int error, const char* description)
 {
--- tests/tests.cpp
+++
@@ -1,6 +0,0 @@
-#define CATCH_CONFIG_MAIN
-#include "Catch2.hpp"
-
-#include "Grid.h"
-#include "Elementary.h"
-#include "GameOfLife.h"
