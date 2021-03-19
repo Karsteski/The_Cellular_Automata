453be869ee5b12d528a234126a9b9a0f161f5e53
Kareem Stephan Skinner <kastsk@gmail.com>
2021-03-19 08:16:58 -0400

    Removed unncessary boilerplate code

--- src/Main.cpp
+++ src/Main.cpp
@@ -22,8 +22,8 @@
 
 // About Desktop OpenGL function loaders:
 //  Modern desktop OpenGL doesn't have a standard portable header file to load OpenGL function pointers.
-//  Helper libraries are often used for this purpose! Here we are supporting a few common ones (gl3w, glew, glad).
-//  You may use another loader/header of your choice (glext, glLoadGen, etc.), or chose to manually implement your own.
+//  Helper libraries are often used for this purpose! Here a few common ones (gl3w, glew, glad).
+//  Another loader may be used (glext, glLoadGen, etc.), or manually implemented.
 #if defined(IMGUI_IMPL_OPENGL_LOADER_GL3W)
 #include <GL/gl3w.h> // Initialize with gl3wInit()
 #elif defined(IMGUI_IMPL_OPENGL_LOADER_GLEW)
@@ -46,15 +46,8 @@ using namespace gl;
 #include IMGUI_IMPL_OPENGL_LOADER_CUSTOM
 #endif
 
-// Include glfw3.h after our OpenGL definitions
+// Include glfw3.h after OpenGL definitions
 #include <GLFW/glfw3.h>
-
-// [Win32] Our example includes a copy of glfw3.lib pre-compiled with VS2010 to maximize ease of testing and compatibility with old VS compilers.
-// To link with VS2010-era libraries, VS2015+ requires linking with legacy_stdio_definitions.lib, which we do using this pragma.
-// Your own project should not be affected, as you are likely to link with a newer binary of GLFW that is adequate for your version of Visual Studio.
-#if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
-#pragma comment(lib, "legacy_stdio_definitions")
-#endif
 
 static void glfw_error_callback(int error, const char* description)
 {
@@ -221,12 +214,7 @@ int main(int, char**)
                     static int gridSteps = 40;
                     ImGui::SetNextItemWidth(100);
                     ImGui::SliderInt("Grid Step Size", &gridSteps, 1, 100);
-                    basicGrid.SetGridSteps(gridSteps);
-
-                    static int basic_rect_x = 0;
-                    static int basic_rect_y = 0;
-
-                    ImGui::SetNextItemWidth(100);
+                  we are supporting   ImGui::SetNextItemWidth(100);
                     ImGui::InputInt("x-coordinate", &basic_rect_x);
                     ImGui::SameLine();
                     ImGui::SetNextItemWidth(100);
